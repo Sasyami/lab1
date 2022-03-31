b0be50d84142627bf4e602a67b0fa56799c56105
Sasyami <93431786+Sasyami@users.noreply.github.com>
2022-03-31 18:32:33 +0300

    Update PolInfo.c

--- PolInfo.c
+++ PolInfo.c
@@ -13,11 +13,12 @@ struct PolInfo{
     void* minus_one;
     void* (*add)(void* , void*);
     char* (*to_string)(void*);
+    int (*comparator)(void*,void*);
 
 };
-struct PolInfo* create_info(size_t size,void* zero,void* (*sum)(void*, void*),void* (*mult)(void*,void*), void* minus_one,void* (*add)(void*,void*),char* (*to_string)(void*) ){
+struct PolInfo* create_info(size_t size,void* zero,void* (*sum)(void*, void*),void* (*mult)(void*,void*), void* minus_one,void* (*add)(void*,void*),char* (*to_string)(void*),int (*comparator)(void*,void*) ){
     struct PolInfo* new_info = malloc(sizeof(struct PolInfo));
-    if ((zero == NULL) || (sum == NULL)|| (mult == NULL)|| (minus_one == NULL)|| (add == NULL)|| (to_string == NULL)){
+    if ((zero == NULL) || (sum == NULL)|| (mult == NULL)|| (minus_one == NULL)|| (add == NULL)|| (to_string == NULL)||(comparator == NULL)){
         return NULL;
     }
     new_info -> size = (size_t)size;
@@ -27,10 +28,11 @@ struct PolInfo* create_info(size_t size,void* zero,void* (*sum)(void*, void*),vo
     new_info -> minus_one = minus_one;
     new_info -> add = add;
     new_info->to_string = to_string;
+    new_info -> comparator = comparator;
     return new_info;
 }
 int info_equal(struct PolInfo* inf1, struct PolInfo* inf2){
-    if ((inf1->mult == inf2->mult)&&(inf1->size == inf2->size)&&(inf1->sum == inf2->sum)){
+    if ((inf1->mult == inf2->mult)&&(inf1->size == inf2->size)&&(inf1->sum == inf2->sum)&&(inf1->add == inf2->add)&&(inf1->to_string == inf2->to_string)&&(inf1->comparator == inf2->comparator)){
         return 1;
     }
     return 0;
